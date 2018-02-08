df0edbb90d9837a5ebd6dc0d46774736f3d5f8e1
jisajose <jissjosekj@gmail.com>
2018-02-08 14:36:01 -0500

    anotherapi

---
+++ FirebaseAuth/app/src/main/java/info/androidhive/firebase/MakeAppointment.java
@@ -0,0 +1,13 @@
+package info.androidhive.firebase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MakeAppointment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_appointment);
+    }
+}
